c6da6fdcd166c4e873f0b018d32af3d50bbb0cec
Raviteja4438 <ravitejaanumula1@gmail.com>
2024-08-10 11:22:15 +0530

    levon techno

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import "./index.css";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <p>© 2023 Horizon UI. All Rights Reserved. Made with love by Simmmple.</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './index.css'
+
+const Sidebar = () => {
+    return (
+        <aside className="sidebar">
+            <ul className='sideD'>
+                <li>Overview</li>
+                <li>Analytics</li>
+                <li>Settings</li>
+            </ul>
+        </aside>
+    );
+}
+
+export default Sidebar;
